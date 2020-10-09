aa24cecf4243efa923dfc69a500f744f5866ad5e
Enrico La Sala <enrico.lasala@buildnn.com>
2020-10-09 12:48:01 +0200

    Refactor calcolatrice alternativa

--- lezione2/python/calcolatrice_alternativa.py
+++ lezione2/python/calcolatrice_alternativa.py
@@ -7,29 +7,29 @@ while True:
     -Per effettuare una Divisone, seleziona 4
     -Per uscire dal programma puoi digitare 5''')
 
-    scelta = input("Inserisci il numero corrispondente all\'operazione scelta:")
+    scelta = str(input("Inserisci il numero corrispondente all'operazione scelta:"))
     if scelta == "1":
-        print('\nHai scelto: Addizione\n')
-        a = float(input('Inserisci il Primo Numero: '))
-        b = float(input('Inserisci il Secondo Numero: '))
-        print('Il risultato della Somma è: ' + str(a + b))
+        print("\nHai scelto: Addizione\n")
+        a = float(input("Inserisci il Primo Numero: "))
+        b = float(input("Inserisci il Secondo Numero: "))
+        print("Il risultato della Somma e': " + str(a + b))
     elif scelta == "2":
-        print('\nHai Scelto: Sottrazione\n')
-        a = float(input('Inserisci il Primo Numero: '))
-        b = float(input('Inserisci il Secondo Numero: '))
-        print('Il risultato della Sottrazione è: ' + str(a - b))
+        print("\nHai Scelto: Sottrazione\n")
+        a = float(input("Inserisci il Primo Numero: "))
+        b = float(input("Inserisci il Secondo Numero: "))
+        print("Il risultato della Sottrazione e': " + str(a - b))
     elif scelta == "3":
-        print('\nHai scelto: Moltiplicazione\n')
-        a = float(input('Inserisci il Primo Numero: '))
-        b = float(input('Inserisci il Secondo Numero: '))
-        print('Il risultato della Moltiplicazione è: ' + str(a * b))
+        print("\nHai scelto: Moltiplicazione\n")
+        a = float(input("Inserisci il Primo Numero: "))
+        b = float(input("Inserisci il Secondo Numero: "))
+        print("Il risultato della Moltiplicazione e': " + str(a * b))
     elif scelta == "4":
-        print('\nHai scelto: Divisione\n')
-        a = float(input('Inserisci il Primo Numero: '))
-        b = float(input('Inserisci il Secondo Numero: '))
-        print('Il risultato della Divisione è: ' + str(a / b))
+        print("\nHai scelto: Divisione\n")
+        a = float(input("Inserisci il Primo Numero: "))
+        b = float(input("Inserisci il Secondo Numero: "))
+        print("Il risultato della Divisione e': " + str(a / b))
     elif scelta == "5":
-        print("L'applicazione verrà ora chiusa!")
+        print("L'applicazione verra' ora chiusa!")
         break
 
     loop = input('Desideri continuare ad usare l\'applicazione? S/N ')
